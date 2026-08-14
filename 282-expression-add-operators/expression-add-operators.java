210bb121c502d899fd103c35a7e16038c97b6e60
vukkem manoj kumar <manojvukkem@gmail.com>
2026-08-14 11:11:58 +0530

    Time: 42 ms (97.79%) | Memory: 45 MB (99.88%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.java
+++ 282-expression-add-operators/expression-add-operators.java
@@ -2,25 +2,36 @@ class Solution {
     public List<String> addOperators(String num, int target) {
         List<String>ans=new ArrayList<>();
         if(num==null||num.isEmpty())return ans;
-        helper("",0,0,0,num,target,ans);
+        StringBuilder sb=new StringBuilder();
+        helper(sb,0,0,0,num,target,ans);
         return ans;
     }
-    private void helper(String curr,int idx,long eval,long prev,String num,int target,List<String>ans){
+    private void helper(StringBuilder sb,int idx,long eval,long prev,String num,int target,List<String>ans){
         if(idx==num.length()){
             if(eval==target){
-                ans.add(curr);
+                ans.add(sb.toString());
             }
             return;
         }
+        long currnum=0;
+        int len=sb.length();
         for(int i=idx;i<num.length();i++){
             if(i>idx && num.charAt(idx)=='0')break;
-            long currnum=Long.parseLong(num.substring(idx,i+1));
+            currnum=currnum*10+(num.charAt(i)-'0');
             if(idx==0){
-                helper(curr+currnum,i+1,currnum,currnum,num,target,ans);
+                sb.append(currnum);
+                helper(sb,i+1,currnum,currnum,num,target,ans);
+                sb.setLength(len);
             }else{
-                helper(curr+"+"+currnum,i+1,eval+currnum,currnum,num,target,ans);
-                helper(curr+"-"+currnum,i+1,eval-currnum,-currnum,num,target,ans);
-                helper(curr+"*"+currnum,i+1,(eval-prev)+(prev*currnum),prev*currnum,num,target,ans);
+                sb.append('+').append(currnum);
+                helper(sb,i+1,eval+currnum,currnum,num,target,ans);
+                sb.setLength(len);
+                sb.append('-').append(currnum);
+                helper(sb,i+1,eval-currnum,-currnum,num,target,ans);
+                sb.setLength(len);
+                sb.append('*').append(currnum);
+                helper(sb,i+1,(eval-prev)+(prev*currnum),prev*currnum,num,target,ans);
+                sb.setLength(len);
             }
         }
     }
