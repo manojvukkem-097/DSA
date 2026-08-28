026a60f892fd4555fdaeb04716ae2ce96b3d082d
vukkem manoj kumar <manojvukkem@gmail.com>
2026-08-28 20:40:10 +0530

    Time: 2 ms (97.80%) | Memory: 48.4 MB (22.25%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,36 +1,20 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int time=0;
         int[]map=new int[26];
         for(char ch:tasks){
             map[ch-'A']++;
         }
-        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=0;i<26;i++){
-            if(map[i]>0){
-                pq.add(map[i]);
+        int maxfreq=0;
+        for(int num:map){
+            maxfreq=Math.max(maxfreq,num);
+        }
+        int maxcount=0;
+        for(int num:map){
+            if(num==maxfreq){
+                maxcount++;
             }
         }
-        while(!pq.isEmpty()){
-            List<Integer>temp=new ArrayList<>();
-            for(int i=1;i<=n+1;i++){
-                if(!pq.isEmpty()){
-                    int freq=pq.poll();
-                    freq--;
-                    temp.add(freq);
-                }
-            }
-            for(int i=0;i<temp.size();i++){
-                if(temp.get(i)>0){
-                    pq.add(temp.get(i));
-                }
-            }
-            if(!pq.isEmpty()){
-                time+=n+1;
-            }else{
-                time+=temp.size();
-            }
-        }
-        return time;
+        int time=(maxfreq-1)*(n+1)+maxcount;
+        return Math.max(time,tasks.length);
     }
 }
