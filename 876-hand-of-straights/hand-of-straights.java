c71b82e88efe0eb1b308445e46e6b75e80067f7d
vukkem manoj kumar <manojvukkem@gmail.com>
2026-08-28 21:55:29 +0530

    Time: 33 ms (68.24%) | Memory: 48.1 MB (51.23%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int num:hand){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        PriorityQueue<Integer>pq=new PriorityQueue<>(map.keySet());
+        while(!pq.isEmpty()){
+            int first=pq.peek();
+            for(int i=0;i<groupSize;i++){
+                int curr=first+i;
+                if(!map.containsKey(curr)){
+                    return false;
+                }
+                int freq=map.get(curr);
+                if(freq==1){
+                    map.remove(curr);
+                }else{
+                    map.put(curr,freq-1);
+                }
+            }
+            while(!pq.isEmpty()&& !map.containsKey(pq.peek())){
+                pq.poll();
+            }
+        }
+        return true;
+    }
+}
